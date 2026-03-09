2cc2511f9669844aa2b1bfcfc677c7933008fa8e
Priya-06-12 <priyaangel61003@gmail.com>
2026-03-09 12:55:42 +0530

    added cards

--- src/data/events.ts
+++ src/data/events.ts
@@ -13,7 +13,7 @@ export const events = [
     title: "Pitch-X",
     description:
       "A platform for student startups and alumni (up to 3 years) startups to pitch their business plans to a panel of investors, providing opportunities for funding, mentorship, and networking.",
-    eventzgoUrl: "https://www.eventzgo.com/events/j57ah57hny092mffkn12xnnern827n9b",
+    // eventzgoUrl: "https://www.eventzgo.com/events/j57ah57hny092mffkn12xnnern827n9b",
   },
 
   {
@@ -21,7 +21,7 @@ export const events = [
     title: "Faculty Pitch",
     description:
       "To recognize and celebrate the entrepreneurial spirit within academia. This award will highlight successful faculty-led startups that have made significant contributions to innovation and societal impact.",
-    eventzgoUrl: "https://www.eventzgo.com/events/j573snp9c6mn44zqmdw8pqw8pn827nfk",
+    // eventzgoUrl: "https://www.eventzgo.com/events/j573snp9c6mn44zqmdw8pqw8pn827nfk",
   },
 
   {
@@ -29,7 +29,7 @@ export const events = [
     title: "Social X – Innovation for Impact",
     description:
       "A platform bridging social innovation and entrepreneurship, bringing together change-makers, startups, and impact investors to co-create solutions for real-world challenges.",
-    eventzgoUrl: "https://www.eventzgo.com/events/j575txsvh8zt9q8qy556bjfph9827az8",
+    // eventzgoUrl: "https://www.eventzgo.com/events/j575txsvh8zt9q8qy556bjfph9827az8",
   },
 
   {
@@ -44,42 +44,46 @@ export const events = [
     title: "Startup Mughavari",
     description:
       "Aimed at helping students with validated innovations to register their startups, the startup sprint drive will be held during the conclave to assist in different stages (early-stage ideas, mid-stage validation, incorporation) of company formation and patent filing.",
-    eventzgoUrl: "https://www.eventzgo.com/events/j57bq65wzwcb1gpaj9tfzvfbw5826wqt",
-  },
-  {
-    id: "e-cell-awards",
-    title: "Golden Star E-Cell Awards",
-    description:
-      "Awards to recognize colleges with exceptional Entrepreneurship Cells that have demonstrated outstanding performance in promoting entrepreneurship activities. Award Categories: 1. Super Star 2. Rising Star",
-    buttonText: "Stay Tuned"
-  },
-
-  {
-    id: "intern-hunt",
-    title: "Intern Hunt",
-    description:
-      "To bridge the gap between startups and talented students seeking internships. This event will provide a platform for startups to showcase their needs and for students to find opportunities that align with their career goals.",
 
   },
 
-  {
-    id: "path-finder",
-    title: "StartUp Path Finder",
-    description:
-      "To explore the role of AI in guiding startups toward success. This panel discussion will bring together AI experts and successful entrepreneurs to discuss AI-driven mentorship and decision-making.",
 
-  },
   {
-    id: "startup-district",
-    title: "Startup District",
+    id: "lab2launch",
+    title: "Lab2Launch",
     description:
       "To provide startups with a platform to showcase their products and innovations to potential investors, customers, and partners. One team/incubator in Tamil Nadu will be offered a reserved slot in the startup showroom. This event will serve as a launchpad for scaling up.",
 
   },
 
   {
-    id: "lab2launch",
-    title: "Lab2Launch",
+    id: "Research-to-Startup",
+    title: "Research-to-Startup",
+    description:
+      "To provide startups with a platform to showcase their products and innovations to potential investors, customers, and partners. One team/incubator in Tamil Nadu will be offered a reserved slot in the startup showroom. This event will serve as a launchpad for scaling up.",
+
+  },
+
+  {
+    id: "kadAI",
+    title: "kadAI",
+    description:
+      "To provide startups with a platform to showcase their products and innovations to potential investors, customers, and partners. One team/incubator in Tamil Nadu will be offered a reserved slot in the startup showroom. This event will serve as a launchpad for scaling up.",
+
+  },
+
+  {
+    id: "IMPRENT",
+    title: "IMPRENT",
+    description:
+      "To provide startups with a platform to showcase their products and innovations to potential investors, customers, and partners. One team/incubator in Tamil Nadu will be offered a reserved slot in the startup showroom. This event will serve as a launchpad for scaling up.",
+
+  },
+
+
+  {
+    id: "Industry Grand Challenge",
+    title: "Industry Grand Challenge",
     description:
       "To provide startups with a platform to showcase their products and innovations to potential investors, customers, and partners. One team/incubator in Tamil Nadu will be offered a reserved slot in the startup showroom. This event will serve as a launchpad for scaling up.",
 
